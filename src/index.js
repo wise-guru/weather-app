68cad704c67cae64e090eccfe8693139ce7d3e5b
wise-guru <wise.guru123@gmail.com>
2022-07-26 20:40:05 -0400

    Add js validation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,68 @@
 import _ from "lodash";
 import './style.css'
 
-async function getGeoInfo() {
-    const input = 'London'
-0
-    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=' +input+ '&limit=5&appid=680265e3e07eb5d8401e3eef55579493', {mode: 'cors'})
 
+const weatherBtn = document.querySelector('#weatherBtn')
+const userInput = document.querySelector('#locationInput')
 
-    const geoData = await response.json()
+weatherBtn.addEventListener('click', function(e) {
+    validateForm(e)
+})
 
-    console.log(geoData)
+function validateForm(e) {
+    e.preventDefault()
 
-    const state = geoData[0].state;
-    const longitude = geoData[0].lon;
-    const latitude = geoData[0].lat;
+    if (userInput.validity.valueMissing) {
+        userInput.setCustomValidity('Please enter a city.')
+        userInput.reportValidity();
+        
+    } else {
+        userInput.setCustomValidity('')
+        getGeoInfo(userInput.value)
+    }
 
-    console.log(longitude)
-    console.log(state)
+  
+   
 }
 
-getGeoInfo();
+
+async function getGeoInfo(input) {
+
+    try {
+        const getCoordinates = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=' +input+ '&limit=5&appid=680265e3e07eb5d8401e3eef55579493', {mode: 'cors'})
+        const geoData = await getCoordinates.json()
+    
+        const state = geoData[0].state;
+        const longitude = geoData[0].lon;
+        const latitude = geoData[0].lat;
+    
+    
+        const getWeather = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=680265e3e07eb5d8401e3eef55579493', {mode: 'cors'})
+        const weatherData = await getWeather.json()
+        console.log(weatherData)
+    
+        const weatherInfo = {
+            name: `${weatherData.name}, ${state}`,
+            temp: `${weatherData.main.temp}`,
+            feelsLike: `Feels like: ${weatherData.main.feels_like}`,
+            mainWeather: weatherData.weather[0].main,
+            humidity: `Humidity: ${weatherData.main.humidity}%`,
+            windSpeed: `${weatherData.wind.speed} k/m`
+        }
+    
+    
+        console.log(weatherInfo)
+    }
+    catch(error) {
+        userInput.setCustomValidity('Please enter a valid city.')
+        userInput.reportValidity();
+    }
+
+
+
+
+}
+
+function displayWeatherInfo() {
+
+}
